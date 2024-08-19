3150b818cdb026cb46947f8714e2872d24bcd49d
sebehart <sebehart@gmail.com>
2024-08-19 10:03:55 +0100

    server fix

--- server.mjs
+++ server.mjs
@@ -1,6 +1,10 @@
-const WebTorrent = require('webtorrent-hybrid');
-const express = require('express');
-const path = require('path');
+import WebTorrent from 'webtorrent-hybrid';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const client = new WebTorrent();
